9e2366bd4e936ff645fc6420a7c98f8608a808ff
S P H K <ponharishkumars.21cse@kongu.edu>
2024-02-21 22:10:10 +0530

    SignIn api created

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,44 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 export const signup =async (req,res,next)=>{
-    const {email,password}=req.body;
+    
+    const {username,email,password}=req.body;
+    
     const hashpass=bcryptjs.hashSync(password,10);
-    const newUser=new User({email,password:hashpass});
+    
+    const newUser=new User({username,email,password:hashpass});
+    
     try{
+        
         await newUser.save();
-       // console.log(req.body)
+        // console.log(req.body)
         res.status(201).json("User created successfully");
     }catch(error){
         next(errorHandler(500,error.message));
     }
    // res.json(newUser);
 };
+
+
+
+
+export const signin= async(req,res,next)=>{
+    const {email,password}=req.body;
+    try {
+        const validUser=await User.findOne({email});
+        if(!validUser) return next(errorHandler(404,"User not found"));
+        const validpass=bcryptjs.compareSync(password,validUser.password)
+        if(!validpass) return next(errorHandler(404,"Invalid credentials"));
+
+
+        const token =jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+        const{password: pass,...rest}=validUser._doc;
+
+        res.cookie('token1',token,{httpOnly:true}).status(200).json(rest);
+    } catch (error) {
+        next(error)
+    }
+
+}
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { signup } from "../controllers/auth.controller.js";
+import { signin } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
@@ -9,5 +10,7 @@ router
 // router
 // .route('/')    
 // .post("signup",signup);
+
+router.route('/signin').post(signin);
 
 export default router;
