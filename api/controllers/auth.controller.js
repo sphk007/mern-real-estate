9a4a26235a0d6047e5fd0edcb8411270aa741a7d
S P H K <ponharishkumars.21cse@kongu.edu>
2024-02-21 14:27:53 +0530

    Navbar Signup fully completed and connected to DB

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,16 @@
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+export const signup =async (req,res,next)=>{
+    const {email,password}=req.body;
+    const hashpass=bcryptjs.hashSync(password,10);
+    const newUser=new User({email,password:hashpass});
+    try{
+        await newUser.save();
+       // console.log(req.body)
+        res.status(201).json("User created successfully");
+    }catch(error){
+        next(errorHandler(500,error.message));
+    }
+   // res.json(newUser);
+};
--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from"./routes/auth.route.js";
+
+import cors from "cors"
+
 dotenv.config();
+
 mongoose.connect(process.env.MONGO).then(( )=>{
     console.log("connected");
 }).catch((err)=>{
@@ -10,6 +16,28 @@ mongoose.connect(process.env.MONGO).then(( )=>{
 
 const app=express();
 
-app.listen(3000,()=>{
+app.use(express.json());
+app.use(cors());
+
+
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
+
+// app.post('/api/auth/signup',(req,res,next)=>{
+//     console.log(req.body)
+// })
+
+//middle Ware//
+app.use((err,req,res,next)=>{
+    
+    const statusCode=err.statusCode || 500;
+    const message=err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+app.listen(4000,()=>{
     console.log("Server");
 })
