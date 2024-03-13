18c27b216e138e2363b2abbd6d79d5c9dc30bc0c
nithesh407 <nitheshravikumar13631@gmail.com>
2024-03-13 21:20:08 +0530

    added cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://home-sphere.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     next();
 })
 
