bdc1e8df5761f814a88678d412952af6e8fd2062
nithesh407 <nitheshravikumar13631@gmail.com>
2024-03-13 20:53:38 +0530

    added vercel.json

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
-import authRouter from"./routes/auth.route.js";
-import listRouter from"./routes/list.route.js";
+import authRouter from "./routes/auth.route.js";
+import listRouter from "./routes/list.route.js";
 import cookieParser from "cookie-parser";
 // import cors from "cors";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(( )=>{
+mongoose.connect(process.env.MONGO).then(() => {
     console.log("connected");
-}).catch((err)=>{
+}).catch((err) => {
     console.log("Error" + err);
-}); 
+});
 
 
-const app=express();
+const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -24,20 +24,25 @@ app.use(cookieParser());
 
 
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
-app.use('/api/listing',listRouter);
-
-app.use((err,req,res,next)=>{
-    const statusCode=err.statusCode || 500;
-    const message=err.message || "Internal server error";
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/listing', listRouter);
+app.use('/test', (req, res, next) => {
+    res.status(200).json({
+        status: "success",
+        message: "hello from api"
+    })
+})
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
     return res.status(statusCode).json({
         success: false,
         statusCode,
         message,
     });
 });
-app.listen(4000,()=>{
+app.listen(4000, () => {
     console.log("Server");
 })
 
