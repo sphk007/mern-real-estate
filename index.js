cb547dbde37660196124ab62beabfd9642ca944b
nithesh407 <nitheshravikumar13631@gmail.com>
2024-03-13 21:21:31 +0530

    added cors

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.use(cors());
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
     next();
 })
 
